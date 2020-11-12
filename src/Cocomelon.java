4d004c59411a9f99cda92a1dfa68d2b5bd4a9a00
Nurlan <nurlanbilalov@gmail.com>
2020-11-12 11:32:22 -0500

    Johnny Johnny

--- src/Cocomelon.java
+++ src/Cocomelon.java
@@ -3,5 +3,6 @@ public class Cocomelon {
         System.out.println("What is Cocomelon ?");
         System.out.println("Coco"+"melon");
         // Are we there yer
+        // Johnny Johnny  yes papa
     }
 }
