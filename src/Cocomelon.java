623c6de4024f0752c784ccf8b486f5dea8fb6282
Nurlan <nurlanbilalov@gmail.com>
2020-11-12 11:31:44 -0500

    are we there yet?

--- src/Cocomelon.java
+++ src/Cocomelon.java
@@ -2,5 +2,6 @@ public class Cocomelon {
     public static void main(String[] args) {
         System.out.println("What is Cocomelon ?");
         System.out.println("Coco"+"melon");
+        // Are we there yer
     }
 }
